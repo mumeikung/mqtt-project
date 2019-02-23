0d25d4c1be92ed3096bb331a35418f12d6d55ddc
Phumipak Jungpakdee <mumeikung@Shiba-Macbook.local>
2019-02-24 02:25:08 +0700

    Broker Ready to Demo

--- broker/broker.js
+++ broker/broker.js
@@ -1,18 +1,20 @@
 const net = require('net')
 const UUID = require('uuidv4')
 
+const debug = true
+
 let JNCFList = {}
 let topicList = {}
 
 const server = net.createServer((socket) => {
   const socketId = UUID()
   const address = socket.address()
-  console.log('socket id:', socketId)
-  console.log('address:', address.address, 'port:', address.port, 'family:', address.family)
+  if (debug) console.log('socket id:', socketId, 'is start')
+  if (debug) console.log('address:', address.address, 'port:', address.port, 'family:', address.family)
   const myJNCF = new JNCF(socket, socketId)
   JNCFList[socketId] = myJNCF
   socket.on('data', (buffer) => {
-    debugBuffer(buffer)
+    // debugBuffer(buffer)
     try {
       myJNCF.decode(buffer)
     } catch (error) {
@@ -20,18 +22,22 @@ const server = net.createServer((socket) => {
     }
   })
   socket.on('end', () => {
-    console.log('end')
+    if (debug) console.log('end')
+    if (debug) console.log('socket id:', socketId, 'is ended')
     myJNCF.END()
   })
   socket.on('close', (error) => {
-    console.log('close isError:', error)
+    if (debug) console.log('close isError:', error)
+    if (debug) console.log('socket id:', socketId, 'is closed')
     myJNCF.END()
   })
   socket.on('timeout', () => {
-    console.log('timeout')
+    if (debug) console.log('timeout')
+    if (debug) console.log('socket id:', socketId, 'is timeout')
     myJNCF.END()
   })
   socket.on('error', (error) => {
+    if (debug) console.error('socket id:', socketId, 'error!')
     console.error('error', error.name, error.message)
     myJNCF.END()
   })
@@ -54,92 +60,96 @@ class JNCF {
     // type 1 3 4 5 7
     if (flags !== '0000') throw new Error('Flag not correct')
     const RemainingLength = parseInt(to8bit(buffer[1]) + to8bit(buffer[2]), 2)
-    console.log('Remaining Length:', RemainingLength)
+    if (debug) console.log('Remaining Length:', RemainingLength)
     let nextBit = 3
     if (type === 1) { // CONN Type
-      console.log('Type: CONN')
+      if (debug) console.log('Type: CONN')
       const protocolNameLength = buffer[nextBit++]
-      console.log('Protocol Name Length:', protocolNameLength)
+      if (debug) console.log('Protocol Name Length:', protocolNameLength)
       let protocolName = ''
       for (let i = 0; i < protocolNameLength; i++) {
         protocolName += String.fromCharCode(buffer[nextBit++])
       }
       const protocolVersion = buffer[nextBit++]
-      console.log('Protocol Name:', protocolName)
-      console.log('Protocol Version:', protocolVersion)
+      if (debug) console.log('Protocol Name:', protocolName)
+      if (debug) console.log('Protocol Version:', protocolVersion)
       if (protocolName !== 'JNCF') throw new Error('Protocol Name not correct')
       if (protocolVersion !== 1) throw new Error('Protocol Level not correct')
       if (nextBit-3 !== RemainingLength) throw new Error('Remaining Length not correct')
       return this.CONNACK()
-    } else if (type === 3) { // PUB Type
-      console.log('Type: PUB')
+    } else if (!this.isConnect) throw new Error('Socket:', this.socketId, 'not connect!!!')
+    if (type === 3) { // PUB Type
+      if (debug) console.log('Type: PUB')
       const topicLength = buffer[nextBit++]
-      console.log('Topic Length:', topicLength)
+      if (debug) console.log('Topic Length:', topicLength)
       let topic = ''
       for (let i = 0; i < topicLength; i++) {
         topic += String.fromCharCode(buffer[nextBit++])
       }
       topic = topicFormat(topic)
-      console.log('Topic:', topic)
+      if (debug) console.log('Topic:', topic)
       const messageId = to8bit(buffer[nextBit++]) + to8bit(buffer[nextBit++])
-      console.log('Message ID:', messageId, parseInt(messageId, 2))
+      if (debug) console.log('Message ID:', messageId, parseInt(messageId, 2))
       // payload
       let payload = ''
       const payloadLength = RemainingLength - (nextBit - 3)
-      console.log('Payload Length:', payloadLength)
+      if (debug) console.log('Payload Length:', payloadLength)
       for (let i = 0; i < payloadLength; i++) {
-        payload += String.fromCharCode(buffer[nextBit])
+        payload += String.fromCharCode(buffer[nextBit++])
       }
-      console.log('Payload:', payload)
+      if (debug) console.log('Payload:', payload)
       if (nextBit-3 !== RemainingLength) throw new Error('Remaining Length not correct')
-      console.log(`=====>> TOPIC: "${topic}", MESSAGE: "${payload}" <<=====`)
+      console.log(`PUBLISH => TOPIC: "${topic}" => MESSAGE: "${payload}"`)
       this.PUBACK(messageId)
       return pubToSub(topic, payload)
     } else if (type === 4) { // PUBACK
-      console.log('Type: PUBACK')
+      if (debug) console.log('Type: PUBACK')
       const messageId = parseInt((to8bit(buffer[nextBit++]) + to8bit(buffer[nextBit++])), 2)
-      console.log('Message ID:', messageId)
+      if (debug) console.log('Message ID:', messageId)
       if (nextBit-3 !== RemainingLength) throw new Error('Remaining Length not correct')
       if (this.messageId) {
         if (this.messageId === messageId) {
           clearInterval(this.loopPUB)
           delete this.loopPUB
           delete this.messageId
-          console.log('PUBACK Complete')
+          if (debug) console.log('PUBACK Complete')
           return null
         } else throw new Error('Message ID not match')
       } else throw new Error('Message ID not found')
     } else if (type === 5) { // SUB
-      console.log('Type: SUB')
+      if (debug) console.log('Type: SUB')
       const topicLength = buffer[nextBit++]
-      console.log('Topic Length:', topicLength)
+      if (debug) console.log('Topic Length:', topicLength)
       let topic = ''
       for (let i = 0; i < topicLength; i++) {
         topic += String.fromCharCode(buffer[nextBit++])
       }
       topic = topicFormat(topic)
-      console.log('Topic:', topic)
+      if (debug) console.log('Topic:', topic)
       if (nextBit-3 !== RemainingLength) throw new Error('Remaining Length not correct')
       if (!topicList[topic]) topicList[topic] = {}
-      topicList[topic][socketId] = true
+      topicList[topic][this.socketId] = true
       this.topic = topic
       return this.SUBACK()
     } else if (type === 7) { // PING
-      console.log('Type: PING')
+      if (debug) console.log('Type: PING')
       return this.PINGACK()
     }
     throw new Error ('Type not correct')
   }
 
   CONNACK () {
-    console.log('CONNACK')
+    if (debug) console.log('CONNACK')
+    this.isConnect = true
     const ackHeader = [32, 0, 1, 0]
+    if (debug) console.log('===== END =====')
     return this.socket.write(new Buffer(ackHeader))
   }
 
   PUB (pubData = pubBuffer()) {
-    console.log('PUB')
+    if (debug) console.log('PUB')
     this.waitPUBACK(pubData)
+    if (debug) console.log('===== END =====')
     return this.socket.write(pubData.buffer)
   }
 
@@ -151,26 +161,30 @@ class JNCF {
   }
 
   PUBACK (messageId = '') {
-    console.log('PUBACK')
+    if (debug) console.log('PUBACK')
     const msgId = ('0000000000000000' + messageId).substr(-16)
     const ackHeader = [64, 0, 2, parseInt(msgId.substr(0, 8), 2), parseInt(msgId.substr(8, 8), 2)]
+    if (debug) console.log('===== END =====')
     return this.socket.write(new Buffer(ackHeader))
   }
 
   SUBACK () {
-    console.log('SUBACK')
+    if (debug) console.log('SUBACK')
     const ackHeader = [96, 0, 1, 0]
+    if (debug) console.log('===== END =====')
     return this.socket.write(new Buffer(ackHeader))
   }
 
   PINGACK () {
-    console.log('PINGACK')
+    if (debug) console.log('PINGACK')
     const ackHeader = [128, 0, 0]
+    if (debug) console.log('===== END =====')
     return this.socket.write(new Buffer(ackHeader))
   }
 
   END () {
     if (this.isEnd) return null
+    this.isConnect = false
     this.socket.end()
     if (this.topic) {
       // unset me in topic
@@ -178,6 +192,8 @@ class JNCF {
     }
     delete JNCFList[this.socketId]
     this.isEnd = true
+    this.socket.destroy()
+    if (debug) console.log('===== END SOCKET =====')
   }
 }
 
@@ -186,9 +202,9 @@ const pubToSub = (topic = '', payload = '') => {
   const list = topicList[myTopic]
   if (list) {
     const pubData = pubBuffer(topic, payload)
-    for (let i = 0; i < list.length; i++) {
-      const thisJNCF = JNCFList[list[i]]
-      if (thisJNCF) thisJNCF.PUB(pubData)
+    for (const key in list) {
+      const thisJNCF = JNCFList[key]
+      if (thisJNCF) thisJNCF.PUB(pubData)   
     }
   }
 }
