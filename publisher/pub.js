ec41625f8afd68fa12013005b4db19cf071a8698
Phumipak Jungpakdee <mumeikung@Shiba-Macbook.local>
2019-02-27 20:10:44 +0700

    Edit Code PUB v1

--- publisher/pub.js
+++ publisher/pub.js
@@ -1,86 +1,67 @@
 #!/usr/bin/env node
 'use strict'
+
 const net = require('net');
 const con = new net.Socket();
-const topic = process.argv[3] || '/'
 const ip = process.argv[2] || 'localhost'
+const topic = process.argv[3] || '/'
+const data = process.argv[4] || ''
+
 function getRandomInt(max) 
 {
-    const val = Math.floor(Math.random() * Math.floor(max))
-    return val
+    return Math.floor(Math.random() * Math.floor(max))
 }
-function addInfoToBuffer(value = [], r = 0)
+
+function addInfoToBuffer(value = [], check = 0)
 {
-    let check = r
     let beforeBuff16Bit = ''
-    let buff16Bit = 0 
-    let buff8Bit1 = 0
-    let buff8Bit2 = 0
+    let buff16Bit = ''
     let buffLenght = ''
-    let zero16Bit = 0
-    let dec1 = ''
-    let dec2 = ''
-    if(check == 1)
-    {
-        buffLenght = value.toString(2)
-    }
-    else
-    {
-        buffLenght = value.length.toString(2)
-    }
-    zero16Bit = 16 - buffLenght.length
-    for(let k = 0;k<zero16Bit;k++)
+
+    if(check === 1) buffLenght = value.toString(2)
+    else buffLenght = value.length.toString(2)
+
+    const zero16Bit = 16 - buffLenght.length
+    for(let k = 0; k < zero16Bit; k++)
     {
         beforeBuff16Bit = beforeBuff16Bit + '0'
     }
+
     buff16Bit = beforeBuff16Bit + buffLenght
-    buff8Bit1 = buff16Bit.substring(0,8)
-    dec1 = parseInt(buff8Bit1,2)
-    buff8Bit2 = buff16Bit.substring(8,16)
-    dec2 = parseInt(buff8Bit2,2)
     return {
-        dec1: dec1,
-        dec2: dec2
+        dec1: parseInt(buff16Bit.substring(0,8), 2),
+        dec2: parseInt(buff16Bit.substring(8,16), 2)
     }
 }
-con.connect(1883,ip, function() {
-    let arrayTopic = new Array()
-    let buff = []
-    let data = process.argv[4]  || ''
-    let arrayData = new Array()
+
+con.connect(1883, ip, () => {
     let round = 0
-    let mID = 0
-    let dec = ''
-    let mIdToBuff = ''
-        con.write(Buffer.from( [ 16,0,6,4,74,78,67,70,1 ] ))
-        con.on('data' , (buffer) => {    
-        if(buffer[0] == 64)
+    let messageID = 0
+    con.write(Buffer.from([ 16, 0, 6, 4, 74, 78, 67, 70, 1 ])) // CONN
+    con.on('data', (buffer) => {
+        if(buffer[0] === 64)
         {
             round = 1
-            con.write(Buffer.from([144,0,1,0]))
-            con.end()
+            con.end(Buffer.from([ 144, 0, 1, 0 ])) // DISCONN
         }
-        else if(buffer[0] == 32)
+        else if(buffer[0] === 32)
         {
+            let buff = []
             buff.push(topic.length)
-            for(let i=0;i<topic.length;i++)
+            for(let i = 0; i < topic.length; i++)
             {
-                arrayTopic[i] = topic.charCodeAt(i)
-                buff.push(arrayTopic[i])
+                buff.push(topic.charCodeAt(i))
             }
-            mID = getRandomInt(65536,1)
-            mIdToBuff = addInfoToBuffer(mID,1)
+            messageID = getRandomInt(65536, 1)
+            const mIdToBuff = addInfoToBuffer(messageID, 1)
             buff.push(mIdToBuff.dec1)
             buff.push(mIdToBuff.dec2)
-            for(let j=0;j<data.length;j++)
+            for(let j = 0; j < data.length; j++)
             {
-                arrayData[j] = data.charCodeAt(j)
-                buff.push(arrayData[j])
+                buff.push(data.charCodeAt(j))
             }
-            dec = addInfoToBuffer(buff,2)
-            buff.splice(0,0,dec.dec1)
-            buff.splice(1,0,dec.dec2)
-            buff.splice(0,0,48)
+            const remaining2Byte = addInfoToBuffer(buff, 2)
+            buff.splice(0, 0, 48, remaining2Byte.dec1, remaining2Byte.dec2)
             con.write(Buffer.from(buff))
         }
         else if(round != 1)
